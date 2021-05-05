cb53e2c97509c657fa366b470546db4f3ec20878
senanurkotankiran <77417875+senanurkotankiran@users.noreply.github.com>
2021-05-05 18:22:20 +0300

    .

---
+++ javaReactDay3Hw/src/javaReactDay3Hw/InstructorManager.java
@@ -0,0 +1,12 @@
+package javaReactDay3Hw;
+
+public class InstructorManager  extends UserManager{
+
+	public void add(Instructor ınstructor) {
+		System.out.println("New assignments have been added to the site: " + ınstructor.getHomeWork());
+	}
+	
+	public void update(Instructor ınstructor) {
+		System.out.println("Course information uploaded: " + ınstructor.getCourseInformation());
+	}
+}
